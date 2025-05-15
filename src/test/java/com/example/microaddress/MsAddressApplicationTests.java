64923d8d8bc7a25434b9aa8c0ff45b04fd7c84e9
GowthamiVaddi <vaddigowthami9959@gmail.com>
2025-05-15 23:31:32 +0530

    adding git lab file

--- src/test/java/com/example/microaddress/MsAddressApplicationTests.java
+++ src/test/java/com/example/microaddress/MsAddressApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsAddressApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
